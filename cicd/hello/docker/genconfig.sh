51118c2510d69389970dd3c5ea5f02ab93fd9736
symplesims <symplesims@gmail.com>
2022-12-13 12:14:04 +0900

    더미-애플리케이션(helloapi) 이미지 빌드 및 ECR 에 Push 하는 샘플 추가

--- cicd/hello/docker/genconfig.sh
+++ cicd/hello/docker/genconfig.sh
@@ -1,6 +1,3 @@
-#sed -i "s#{listen_port}#$CONTAINER_PORT#g" /tmp/nginx.conf
-#sed -i "s#{health_check_path}#$HEALTH_CHECK_PATH#g" /tmp/nginx.conf
-#sed -i "s#{welcome_message}#$CONTAINER_NAME#g" /tmp/nginx.conf
 cat <<EOF> /tmp/nginx.conf
 events {}
 
